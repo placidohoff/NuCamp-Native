b57be3d607de648494c09abd122e8de620fdb4e3
placidohoff <67982091+placidohoff@users.noreply.github.com>
2021-11-09 20:40:44 -0500

    Side Drawer works

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,34 +1,77 @@
 import React, { Component } from 'react'
+import Home from './HomeComponent'
 import Directory from './DirectoryComponent'
 import CampsiteInfo from './CampsiteInfoComponent'
-import { View } from 'react-native'
-import { CAMPSITES } from '../shared/campsites'
+import Constants from 'expo-constants'
+import { View, Platform } from 'react-native'
+import { createStackNavigator } from 'react-navigation-stack'
+import { createDrawerNavigator } from 'react-navigation-drawer'
+import { createAppContainer } from 'react-navigation'
 
-class Main extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            campsites: CAMPSITES,
-            selectedCampsite: null
+const DirectoryNavigator = createStackNavigator(
+    {
+        Directory: { screen: Directory },
+        CampsiteInfo: { screen: CampsiteInfo }
+    },
+    {
+        initialRouteName: 'Directory',
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
         }
     }
 
-    onCampsiteSelect(campsiteId) {
-        this.setState({ selectedCampsite: campsiteId })
+
+)
+
+const HomeNavigator = createStackNavigator(
+    {
+        Home: { screen: Home },
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
     }
+)
+
+//Notice we want the components to get mapped to via the navigators
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: { screen: HomeNavigator },
+        Directory: { screen: DirectoryNavigator },
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+)
+
+const AppNavigator = createAppContainer(MainNavigator)
+
+class Main extends Component {
+
 
     render() {
         return (
-            <View style={{ flex: 1 }}>
-                <Directory
-                    campsites={this.state.campsites}
-                    onPress={campsiteId => this.onCampsiteSelect(campsiteId)}
-                />
-                <CampsiteInfo
-                    campsite={this.state.campsites.filter(
-                        campsite => campsite.id === this.state.selectedCampsite
-                    )[0]}
-                />
+            <View
+                style={{
+                    flex: 1,
+                    paddingTop: Platform.OS === 'ios' ? 0 : Constants.stausBarHeight
+                }}
+
+            >
+                <AppNavigator />
             </View>
         )
     }
